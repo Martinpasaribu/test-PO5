1f95e1aa258b66b6286706472bcfc5d2f604d280
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2023-12-26 18:36:23 +0700

    U11

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -84,9 +84,9 @@ export const Register = async (req, res) => {
 
 
         res.cookie('refreshToken', refreshToken, {
-            // httpOnly: true,
+            sameSite:None,
             secure: true,
-            maxAge: 24 * 60 * 60 * 1000,
+            maxAge: 24 * 60 * 60 * 1000
         
         });
         res.json({ accessToken});
