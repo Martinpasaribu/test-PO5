09877a909de5a192e53cd82fe8c133cd8973b78a
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2023-12-27 14:54:38 +0700

    a5

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -14,7 +14,7 @@ const Dashboard = () => {
   
   useEffect(() => {
     refreshToken();
-    getUsers();
+    // getUsers();
   }, []);
 
   const refreshToken = async() => {
@@ -35,26 +35,26 @@ const Dashboard = () => {
     }
   }
 
-const axiosJWT = axios.create();
+// const axiosJWT = axios.create();
 
 
-axiosJWT.interceptors.request.use(async(config) => {
-  const currentDate = new Date();
-  if(expire * 1000 < currentDate.getTime()){
-     const response = await axios.get('https://server-po-5.vercel.app/token');
-    config.headers.Authorization = `Bearer ${response.data.accessToken}`;
-    setToken(response.data.accessToken);
-    const decoded = jwtDecode(response.data.accessToken);
-      setName(decoded.name);
-      setExpire(decoded.exp);
+// axiosJWT.interceptors.request.use(async(config) => {
+//   const currentDate = new Date();
+//   if(expire * 1000 < currentDate.getTime()){
+//      const response = await axios.get('https://server-po-5.vercel.app/token');
+//     config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+//     setToken(response.data.accessToken);
+//     const decoded = jwtDecode(response.data.accessToken);
+//       setName(decoded.name);
+//       setExpire(decoded.exp);
 
-  }
+//   }
 
-  return config;
-}, (error) =>{
-  return Promise.reject(error);
+//   return config;
+// }, (error) =>{
+//   return Promise.reject(error);
 
-});
+// });
 
 
   const getUsers = async() => {
