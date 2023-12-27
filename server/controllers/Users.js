706addb3ec6ec87c99da82d510e5699b4629dafa
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2023-12-27 19:01:35 +0700

    a1

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -81,8 +81,7 @@ export const Register = async (req, res) => {
               refresh_token: refreshToken
             }
         });
-
-        res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, maxAge: 24 * 60 * 60 * 1000 });
+        res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
 
         res.json({ accessToken});
 
